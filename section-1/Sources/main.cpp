329c8e70d1f5a1145c658ed8de81a498f07e113e
Jack Fuge <jack.fuge@gmail.com>
2017-10-11 12:21:47 +1100

    start of project 1

---
+++ section-1/Sources/main.cpp
@@ -0,0 +1,38 @@
+/*
+ ============================================================================
+ * @file    main.cpp (180.ARM_Peripherals/Sources/main.cpp)
+ * @brief   Basic C++ demo using GPIO class
+ *
+ *  Created on: 10/1/2016
+ *      Author: podonoghue
+ ============================================================================
+ */
+#include <stdio.h>
+#include "system.h"
+#include "derivative.h"
+#include "hardware.h"
+
+// Allow access to USBDM methods without USBDM:: prefix
+using namespace USBDM;
+
+/**
+ * See more examples in Snippets directory
+ */
+
+// LED connection - change as required
+using Led   = USBDM::GpioA<2,USBDM::ActiveLow>;
+
+int main() {
+//   printf("Starting\n");
+
+//   printf("SystemBusClock  = %ld\n", ::SystemBusClock);
+//   printf("SystemCoreClock = %ld\n", ::SystemCoreClock);
+
+   Led::setOutput();
+   for(;;) {
+      Led::toggle();
+      USBDM::waitMS(100);
+//      printf("Tick\n");
+   }
+   return 0;
+}
